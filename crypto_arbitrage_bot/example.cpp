333abab5a426ddef0bc7fd78caa092109ad584e2
Roman Wang <romanwang@Romans-MacBook-Pro.local>
2018-09-23 04:08:30 -0400

    Read in API keys

--- crypto_arbitrage_bot/example.cpp
+++ crypto_arbitrage_bot/example.cpp
@@ -2,10 +2,30 @@
 
 #include <cpr/cpr.h>
 #include <json.hpp>
+#include "PicoSHA2/picosha2.h"
+
+#include <fstream>
+
 
 
 int main(int argc, char** argv) {
+    std::string burl = "api.binance.com/api/";
     auto response = cpr::Get(cpr::Url{"api.binance.com/api/v1/ticker/allPrices"});
     auto json = nlohmann::json::parse(response.text);
     std::cout << json.dump(4) << std::endl;
+
+    std::ifstream public_key_file("../key_store/public.txt");
+    std::string public_key;
+    if (public_key_file.good()){
+      std::getline(public_key_file, public_key);
+    }
+
+    std::string secret_key;
+    std::ifstream secret_key_file("../key_store/secret.txt");
+    if (secret_key_file.good()){
+      std::getline(secret_key_file, secret_key);
+    }
+
+    std::cout << public_key << std::endl << secret_key << std::endl;
+
 }
