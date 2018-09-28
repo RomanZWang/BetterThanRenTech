2a1b97aab652d309ecf1424422699391a034f66a
Roman Wang <romanwang@Romans-MacBook-Pro.local>
2018-09-28 02:54:14 -0400

    Add comments

--- crypto_arbitrage_bot/example.cpp
+++ crypto_arbitrage_bot/example.cpp
@@ -18,36 +18,43 @@ long int unix_timestamp()
 }
 
 int main(int argc, char** argv) {
+		// Base URL variable initialization
     std::string burl = "https://api.binance.com";
     std::string ep1 = "/api/v1/ticker/allPrices";
     std::string ep2 = "/api/v3/account";
 		std::string unix_epoch_time_now = std::to_string(unix_timestamp());
     std::string data_query_string = "recvWindow=20000&timestamp=";
 
+		// Test out the API GET request to binance
     auto response = cpr::Get(cpr::Url{"api.binance.com/api/v1/ticker/allPrices"});
     auto json = nlohmann::json::parse(response.text);
     std::cout << json.dump(4) << std::endl;
 
+		// Get keys from hidden key files
     std::ifstream public_key_file("../key_store/public.txt");
     std::string public_key;
     if (public_key_file.good()){
       std::getline(public_key_file, public_key);
     }
-
     std::string secret_key;
     std::ifstream secret_key_file("../key_store/secret.txt");
     if (secret_key_file.good()){
       std::getline(secret_key_file, secret_key);
     }
 
+		// Print out preliminary variables
+		std::cout << "Public Key: " << public_key << std::endl;
+		std::cout << "Hash of Secret Key: " << secret_key_hash << std::endl;
+		std::cout << "Full request url: " << request_url << std::endl;
+
+		// Calculate SHA256 key
 		SHA256 sha256object;
 		std::string secret_key_hash = sha256object(secret_key);
 		std::stringstream russ;
+
+		// Create URL request string
 		russ << burl << ep2 << "?" << data_query_string << "&signature=" << secret_key_hash;
 		std::string request_url = russ.str();
 
-    std::cout << "Public Key: " << public_key << std::endl;
-		std::cout << "Hash of Secret Key: " << secret_key_hash << std::endl;
-		std::cout << "Full request url: " << request_url << std::endl;
 		return 0;
 }
