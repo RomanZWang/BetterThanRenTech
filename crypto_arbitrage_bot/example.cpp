6a6a5c3c6ecfd5b004e5f55e1769d794802c0156
Roman Wang <romanwang@Romans-MacBook-Pro.local>
2018-09-28 02:25:52 -0400

    HMAC-SHA256 Library
    Add submodule and cmake linking instructions to cmakelists files

--- crypto_arbitrage_bot/example.cpp
+++ crypto_arbitrage_bot/example.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
+#include <fstream>
+#include <chrono>
 
 #include <cpr/cpr.h>
 #include <json.hpp>
-#include "PicoSHA2/picosha2.h"
+#include "hash-library/sha256.h"
 
-#include <fstream>
-
-
+void calcAndOutput(const std::string& src){
+	std::cout << "src : \"" << src << "\"\n";
+}
 
 int main(int argc, char** argv) {
-    std::string burl = "api.binance.com/api/";
+    std::string burl = "https://api.binance.com/api";
+    std::string ep1 = "/api/v1/ticker/allPrices";
+    std::string ep2 = "/api/v3/account";
+    std::string data_query_string = "recvWindow=20000&timestamp=1519211811670";
+
     auto response = cpr::Get(cpr::Url{"api.binance.com/api/v1/ticker/allPrices"});
     auto json = nlohmann::json::parse(response.text);
     std::cout << json.dump(4) << std::endl;
@@ -26,6 +32,9 @@ int main(int argc, char** argv) {
       std::getline(secret_key_file, secret_key);
     }
 
-    std::cout << public_key << std::endl << secret_key << std::endl;
+		SHA256 sha256object;
 
+    std::cout << "Public Key: " << public_key << std::endl << "Secret Key: " << secret_key << std::endl;
+		std::cout << "Hash of Secret Key: " << sha256object(secret_key) << std::endl;
+		return 0;
 }
