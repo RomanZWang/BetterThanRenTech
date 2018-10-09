30d0059cc9d6fdec589918bfc5952c0e2e42b2de
Roman Wang <romanwang@Romans-MacBook-Pro.local>
2018-10-09 18:56:06 -0400

    Move commands out of main method, fix constructor

--- crypto_arbitrage_bot/example.cpp
+++ crypto_arbitrage_bot/example.cpp
@@ -66,8 +66,11 @@ class ArbitrageBot {
 			//Class related methods
 			ArbitrageBot(std::vector<std::string> *coinNames):requestLimitSec(200), orderLimitSec(10), orderLimitDay(10000){
 				coinsToTradeVector = coinNames;
+				exNamesForward = new std::vector<std::string>();
+				exNamesBackward = new std::vector<std::string>();
 				updateCoinTradeNames();
 			};
+			//TODO Create destructor
 			void updateCoinTradeNames();
 
 			//API call helper methods
@@ -92,6 +95,7 @@ class ArbitrageBot {
 
 void ArbitrageBot::updateCoinTradeNames(){
 	int numCoins = this->coinsToTradeVector->size();
+
 	for(int i = 0; i<numCoins; i++){
 		exNamesForward->push_back(this->coinsToTradeVector->at(i) + this->coinsToTradeVector->at((i+1)%numCoins));
 		exNamesBackward->push_back(this->coinsToTradeVector->at((numCoins-i)%numCoins) + this->coinsToTradeVector->at(numCoins-i-1));
@@ -108,20 +112,42 @@ long int ArbitrageBot::unix_timestamp(){
     return now;
 }
 
-void calcAndOutput(const std::string& src){
-	std::cout << "src : \"" << src << "\"\n";
-}
-
-long int unix_timestamp(){
-    time_t t = std::time(0);
-    long int now = static_cast<long int> (t);
-    return now;
-}
-
 
 // DUMMY
 double ArbitrageBot::getBalance(std::string ticker){
-	return 0;
+	std::ifstream public_key_file("../key_store/public.txt");
+	std::string public_key;
+	if(public_key_file.good()){
+		std::getline(public_key_file, public_key);
+	}
+	std::string secret_key;
+	std::ifstream secret_key_file("../key_store/secret.txt");
+	if(secret_key_file.good()){
+		std::getline(secret_key_file, secret_key);
+	}
+
+	std::string burl = "https://api.binance.com";
+	std::string ep2 = "/api/v3/account";
+	std::string unix_epoch_time_now = std::to_string(unix_timestamp()*1000);
+	std::string data_query_string = "recvWindow=20000&timestamp=" + unix_epoch_time_now;
+
+	SHA256 sha256object;
+	std::string secret_key_hash = hmac<SHA256>(data_query_string, secret_key);
+
+	std::stringstream russ;
+	russ << burl << ep2 << "?" << data_query_string << "&signature=" << secret_key_hash;
+	std::string request_url = russ.str();
+
+	auto response2 = cpr::Get(
+		cpr::Url{request_url},
+		cpr::Header{{"X-MBX-APIKEY", public_key}}
+	);
+
+	auto json2 = nlohmann::json::parse(response2.text);
+
+	if(json2["balances"]["BTC"])
+		return json2["balances"]["BTC"];
+	return -1;
 }
 
 
@@ -179,59 +205,9 @@ void ArbitrageBot::run(){
 }
 
 int main(int argc, char** argv) {
-		// Base URL variable initialization
-    std::string burl = "https://api.binance.com";
-    std::string ep1 = "/api/v1/ticker/allPrices";
-    std::string ep2 = "/api/v3/account";
-		std::string unix_epoch_time_now = std::to_string(unix_timestamp()*1000);
-		//std::string unix_epoch_time_now = (std::chrono::system_clock::now().time_since_epoch()).count();
-    std::string data_query_string = "recvWindow=20000&timestamp=" + unix_epoch_time_now;
 
-		// Get keys from hidden key files
-    std::ifstream public_key_file("../key_store/public.txt");
-    std::string public_key;
-    if(public_key_file.good()){
-      std::getline(public_key_file, public_key);
-    }
-    std::string secret_key;
-    std::ifstream secret_key_file("../key_store/secret.txt");
-    if(secret_key_file.good()){
-      std::getline(secret_key_file, secret_key);
-    }
-
-		// Calculate SHA256 key
-		SHA256 sha256object;
-		std::string secret_key_hash = hmac<SHA256>(data_query_string, secret_key);
-
-		// Create URL request string
-		std::stringstream russ;
-		russ << burl << ep2 << "?" << data_query_string << "&signature=" << secret_key_hash;
-		std::string request_url = russ.str();
-
-		// Test out the API GET request to binance
-    auto response = cpr::Get(cpr::Url{"api.binance.com/api/v1/ticker/allPrices"});
-    auto json = nlohmann::json::parse(response.text);
-    std::cout << json.dump(4) << std::endl;
-
-		// Try signed API GET request
-		auto response2 = cpr::Get(
-			cpr::Url{request_url},
-			cpr::Header{{"X-MBX-APIKEY", public_key}}
-		);
-		auto json2 = nlohmann::json::parse(response2.text);
-		std::cout << json2.dump(4) << std::endl;
-
-		auto response3 = cpr::Get(
-			cpr::Url{"api.binance.com/api/v1/exchangeInfo"}
-		);
-
-		auto json3 = nlohmann::json::parse(response3.text);
-		std::cout << json3.dump(4) << std::endl;
-
-		// Print out preliminary variables
-		std::cout << "Public Key: " << public_key << std::endl;
-		std::cout << "Hash of Secret Key: " << secret_key_hash << std::endl;
-		std::cout << "Full request url: " << request_url << std::endl;
-
-		return 0;
+	std::vector<std::string> coins{ "BTC", "ETH", "XRP" };
+	ArbitrageBot firstBot(&coins);
+	firstBot.run();
+	return 0;
 }
