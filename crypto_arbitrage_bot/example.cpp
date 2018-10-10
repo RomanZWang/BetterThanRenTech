52b9d380a954be3c100b567b6d51b54efa104aeb
Roman Wang <romanwang@Romans-MacBook-Pro.local>
2018-10-10 00:31:29 -0400

    Develop remaining necessary methods

--- crypto_arbitrage_bot/example.cpp
+++ crypto_arbitrage_bot/example.cpp
@@ -1,9 +1,12 @@
+//Standard libraries
 #include <iostream>
 #include <fstream>
 #include <ctime>
 #include <chrono>
 #include <thread>
+#include <cmath>
 
+//External libraries
 #include <cpr/cpr.h>
 #include <json.hpp>
 #include "hash-library/sha256.h"
@@ -50,8 +53,11 @@ class ArbitrageBot {
 			std::vector<std::string> *exNamesBackward;
 			// NEED NUMCOINS int numCoins = coinsToTradeVector.size();
 		public:
+			//TODO Do we need balance attribute?
+
 			double balance;
 			double fee;
+
 			//api request to get info
 			// exchange limits per second
 			int requestLimitSec;
@@ -64,6 +70,7 @@ class ArbitrageBot {
 			std::chrono::high_resolution_clock::time_point startTime;
 
 			//Class related methods
+			void updateCoinTradeNames();
 			ArbitrageBot(std::vector<std::string> *coinNames):requestLimitSec(200), orderLimitSec(10), orderLimitDay(10000){
 				coinsToTradeVector = coinNames;
 				exNamesForward = new std::vector<std::string>();
@@ -71,11 +78,11 @@ class ArbitrageBot {
 				updateCoinTradeNames();
 			};
 			//TODO Create destructor
-			void updateCoinTradeNames();
 
 			//API call helper methods
-			void calcAndOutput(const std::string& src);
 			long int unix_timestamp();
+			string getKey(std::string path);
+			string getRequestURL(std::string endpoint, std::string data_query_string, bool private);
 			double getBalance(std::string ticker);
 
 			//Panic methods
@@ -83,8 +90,9 @@ class ArbitrageBot {
 			void panic();
 
 			//Trade operation methods
-			bool hasArbitrage();
+			bool hasArbitrage(std::vector<std::string> *tradeTickersVector);
 			bool executeTrade(std::vector<std::string> *tradeTickersVector, double amount);
+			bool executeTriangularTrade(std::vector<std::string> *tradeTickersVector, double amount)
 
 			//TODO modify parameters as necessary
 			bool limitReached(int limitSec, int limitDay);
@@ -102,79 +110,138 @@ void ArbitrageBot::updateCoinTradeNames(){
 	}
 }
 
-void ArbitrageBot::calcAndOutput(const std::string& src){
-	std::cout << "src : \"" << src << "\"\n";
-}
-
 long int ArbitrageBot::unix_timestamp(){
     time_t t = std::time(0);
     long int now = static_cast<long int> (t);
     return now;
 }
 
+string ArbitrageBot::getKey(std::string keyPath){
+	std::ifstream key_file(keyPath);
+	std::string key;
+	if(key_file.good()){
+		std::getline(key_file, key);
+	}
+	return key;
+}
 
-// DUMMY
-double ArbitrageBot::getBalance(std::string ticker){
-	std::ifstream public_key_file("../key_store/public.txt");
-	std::string public_key;
-	if(public_key_file.good()){
-		std::getline(public_key_file, public_key);
-	}
-	std::string secret_key;
-	std::ifstream secret_key_file("../key_store/secret.txt");
-	if(secret_key_file.good()){
-		std::getline(secret_key_file, secret_key);
-	}
+string ArbitrageBot::getRequestURL(std::string endpoint, string data_query_string, bool private){
+
 
 	std::string burl = "https://api.binance.com";
-	std::string ep2 = "/api/v3/account";
-	std::string unix_epoch_time_now = std::to_string(unix_timestamp()*1000);
-	std::string data_query_string = "recvWindow=20000&timestamp=" + unix_epoch_time_now;
 
 	SHA256 sha256object;
 	std::string secret_key_hash = hmac<SHA256>(data_query_string, secret_key);
-
 	std::stringstream russ;
-	russ << burl << ep2 << "?" << data_query_string << "&signature=" << secret_key_hash;
-	std::string request_url = russ.str();
+	russ << burl << endpoint << "?" << data_query_string;
+	if(private){
+		public_key = getKey("../key_store/public.txt");
+		secret_key = getKey("../key_store/secret.txt");
+		russ<<"&signature=" << secret_key_hash;
+	}
+	return russ.str();
+}
 
-	auto response2 = cpr::Get(
+double ArbitrageBot::getBalance(std::string ticker){
+
+	std::string unix_epoch_time_now = std::to_string(unix_timestamp()*1000);
+	std::string data_query_string = "recvWindow=20000&timestamp=" + unix_epoch_time_now;
+	request_url = getRequestURL("/api/v3/account", data_query_string, true);
+
+	auto response = cpr::Get(
 		cpr::Url{request_url},
 		cpr::Header{{"X-MBX-APIKEY", public_key}}
 	);
 
-	auto json2 = nlohmann::json::parse(response2.text);
+	auto json = nlohmann::json::parse(response.text);
 
-	if(json2["balances"]["BTC"])
-		return json2["balances"]["BTC"];
+	//TODO fix index accessors
+	if(json["balances"][ticker])
+		return json["balances"][ticker];
 	return -1;
 }
 
+double ArbitrageBot::getExRate(std::string ticker){
 
-// DUMMY
+	std::string unix_epoch_time_now = std::to_string(unix_timestamp()*1000);
+	std::string data_query_string = "symbol=" + ticker;
+	request_url = getRequestURL("/api/v3/ticker/price", data_query_string, false);
+
+	auto response = cpr::Get(
+		cpr::Url{request_url},
+		cpr::Header{{"X-MBX-APIKEY", public_key}}
+	);
+
+	auto json = nlohmann::json::parse(response.text);
+
+	//TODO fix index accessors
+	return (double) json["price"];
+}
+
+// TODO DUMMY
 bool ArbitrageBot::shouldPanic(){
 	// check for panic conditions
 	return false;
 }
 
-// DUMMY
+// TODO DUMMY
 void ArbitrageBot::panic(){
 	//liquidate assets
 }
 
-// DUMMY
-bool ArbitrageBot::hasArbitrage(){
-	return false;
+bool ArbitrageBot::hasArbitrage(std::vector<std::string> *tradeTickersVector){
+	string baseCurrency = tradeTickersVector->at(0);
+	double totalFee = std::math(1+this->fee,3);
+	double feeFactor = 1/totalFee;
+	double arbitrageFactor = 1;
+	//determines arbitrage for the three currencies designated
+	for(int i = 0; i<tradeTickersVector->size(); i++){
+		arbitrageFactor *= tradeTickersVector->at(i);
+	}
+	arbitrageFactor *= feeFactor;
+	//3 exchange rates multiplied should equal 1 for no arbitrage. Arbitrage exists if product is greater than 1.
+	return arbitrageFactor > 1;
 }
 
 
-// DUMMY
-bool ArbitrageBot::executeTrade(std::vector<std::string> *tradeTickersVector, double amount){
-	return false;
+// TODO DUMMY
+tradeSuccess(auto json){
+	return true;
 }
 
-// DUMMY
+bool ArbitrageBot::executeTrade(string ticker, double amount){
+	std::string unix_epoch_time_now = std::to_string(unix_timestamp()*1000);
+	std::string data_query_string = "symbol=" + ticker + "&side=BUY&type=MARKET&quantity=" + amount + "&timestamp=" + unix_epoch_time_now;
+	request_url = getRequestURL("/api/v3/ticker/price", data_query_string, true);
+
+	auto response = cpr::Post(
+		cpr::Url{request_url},
+		cpr::Header{{"X-MBX-APIKEY", public_key}}
+	);
+
+	auto json = nlohmann::json::parse(response.text);
+	return tradeSuccess(json);
+	//TODO fix index accessors
+
+}
+
+bool ArbitrageBot::executeTriangularTrade(std::vector<std::string> *tradeTickersVector, double amount){
+	double currentBalance;
+	string currentTicker;
+	for(int i = 0; i<tradeTickersVector->size(); i++){
+		currentTicker = tradeTickersVector->at(i);
+		balance = getBalance(currentTicker);
+		while(executeTrade(currentTicker, balance)){
+			balance = getBalance(currentTicker);
+		}
+	}
+	//TODO if timeout value is reached, then return false
+	return true;
+}
+
+// TODO DUMMY
 bool ArbitrageBot::limitReached(int limitSec, int limitDay){
+	//TODO make sure we leave room for a few panic sell trades
 	return false;
 }
 
@@ -185,8 +252,12 @@ void ArbitrageBot::run(){
 	int orders = 0;
 	double balance;
 	// Run bot
+	//TODO Account for condition where a trade does not go through
 	while(!limitReached(orderLimitSec, orderLimitDay)){
-		if(hasArbitrage()){
+		if(shouldPanic()){
+			panic();
+		}
+		else if(hasArbitrage()){
 			balance = getBalance("BTC");
 			executeTrade(exNamesForward, balance);
 			orders +=3;
